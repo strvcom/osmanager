c93944127745da7487a5f38c0b81a585821f2aa4
niek mereu <niek.mereu@strv.com>
2022-11-09 13:08:34 +0100

    feat: add test for data insert to OS

---
+++ tests/osman/opensearch/test_data_insert.py
@@ -0,0 +1,54 @@
+import json 
+import pytest
+
+from osman import Osman, OsmanConfig
+from tests.osman.opensearch.utils import index_handler, get_ids_from_response
+
+index_mapping = "index_mapping.json"
+sample_data = "sample_data.json"
+
+
+with open(index_mapping) as json_file:
+    mapping = json.load(json_file)
+
+with open(sample_data) as json_file:
+    data = json.load(json_file)
+
+
+@pytest.mark.parametrize("index_handler", [mapping], indirect=True)
+def test_data_insert(index_handler):
+    
+     # Get instance of OpenSearchHelper connected to OpenSearch
+    config = OsmanConfig(host_url="http://opensearch-node:9200")
+    o = Osman(config)
+
+    index_name = index_handler
+
+    # Put refresh to True for immediate results
+    o.add_data_to_index(index_name, data, refresh=True)
+
+    # Check that documents in OS are the same as in json
+    input_ids = set([document["id"] for document in data])
+
+    # Obtain documents back from OS and compare their id's to 
+    # local
+    search_results = o.search(index_name, {})
+
+    search_ids = get_ids_from_response(search_results)
+
+    ids_difference = input_ids.difference(search_ids)
+    
+    assert len(ids_difference) == 0      
+
+    # assert all documents in OS are the same as local
+    for document in data:
+        id = document["id"]
+        os_document = [
+            doc["_source"] for doc in search_results["hits"]["hits"] \
+            if doc["_source"]["id"] == id
+        ][0]
+
+        assert document == os_document
+            
+
+
---
+++ tests/osman/opensearch/utils/__init__.py
@@ -0,0 +1,56 @@
+
+
+import pytest
+import time
+
+from osman import Osman, OsmanConfig
+
+def get_ids_from_response(response):
+    if 'hits' not in response:
+        print('Missing `hits` in response')
+        return None
+    
+    if 'hits' not in response['hits']:
+        print('Missing `hits` in response["hits"]')
+        return None
+    
+    if len(response['hits']['hits']) == 0:
+        print('Empty response')
+        return []
+    
+    if 'id' not in response['hits']['hits'][0]['_source']:
+        print('Missing `post_id` in documents')
+        return None
+    
+    return [document['_source']['id'] for document in response['hits']['hits']]
+
+
+@pytest.fixture
+def index_handler(request):
+    """
+    Creates new index, yields index name to test case so it can use it
+    and after the test is done deletes the index
+    """
+    # Get name of the caller function (name of the test)
+    caller_name = request.function.__name__
+
+    # Create index name as `caller_name` + timestamp
+    index_name = caller_name+'_'+str(int(time.time()*1000000))
+
+    # Check if there is parameter with index mapping
+    try:
+        mapping = request.param
+    except:
+        mapping = None
+
+    # Create new index with `index_name` and optional mapping using the helper
+    config = OsmanConfig(host_url="http://opensearch-node:9200")
+    o = Osman(config)
+
+    o.create_index(index_name, mapping)
+
+    # Return name of the index to the test that is using this fixture
+    yield index_name
+
+    # Once the test ends (or crash) this line will be executed
+    o.delete_index(index_name)
