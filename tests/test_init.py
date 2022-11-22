76ab3fc1bdd646b4044d712220543b2397a45ff6
Vladimír Kadlec <115160012+vladimirkadlec-strv@users.noreply.github.com>
2022-11-22 15:28:08 +0100

    chore: fix tests initialization (#46)
    * chore: fix tests initialization
    * chore: add test for init

---
+++ tests/test_init.py
@@ -0,0 +1,9 @@
+import logging
+import os
+import pytest
+
+def test_environment():
+    """
+    Environment variables should be deleted in conftest.py
+    """
+    assert os.environ.get("OPENSEARCH_HOST") is None
